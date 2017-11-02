c9d6ea5f97bb6606c38ff0aab5db5ef59cc73695
Tamzida Azad <1421064@iub.edu.bd>
2017-11-02 05:26:11 +0000

    Project

--- controllers/core.server.controller.js
+++ controllers/core.server.controller.js
@@ -11,7 +11,21 @@ exports.index = function(req, res) {
 };
 
 exports.about = function(req, res) {
-	res.render('./../about.ejs', {
+	res.render('./../public/views/about.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+
+exports.login = function(req, res) {
+	res.render('./../public/views/login.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+
+exports.services = function(req, res) {
+	res.render('./../public/views/services.ejs', {
 		user: req.user || null,
 		request: req
 	});
